4c1df3fae4840c7d465565a03ee794fcd05c2858
BrandonMontesflores <79325611+BrandonMontesflores@users.noreply.github.com>
2021-09-02 21:31:01 -0600

    Semana 7

---
+++ Semana 7/Tarea7/src/main/java/Datos/AccesoaDatos.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Datos;
+
+import Dominio.mdEmpleado;
+import Excepciones.*;
+import java.util.*;
+
+/**
+ *
+ * @author Usuario
+ */
+public interface AccesoaDatos {
+    
+    boolean existe(String nombreArchivo) throws AccesoDatos;
+    public List<mdEmpleado> listar(String nombreArchivo) throws LecturaDatos;
+    void escribir(String Info, String nombreArchivo, boolean anexar)throws EscrituraDatos;
+    public void crear(String nombreArchivo)throws AccesoDatos;
+    public void borrar(String nombreArchivo)throws AccesoDatos;     
+}
